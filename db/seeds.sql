954efc6c615ad3f434829e73582edc537f2c0883
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-01-26 20:20:58 -0800

    adding more comments

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,8 +18,19 @@ VALUES
 INSERT INTO employees (first_name, last_name, role_id, manager_id, is_manager)
 VALUES 
     ("Mohamed", "Farah", 3, NULL, true),
-    ('Khabib', 'Nurmagomedov', 2, 2, false),
-    ('Harvey', 'Specter', 2, 1, false);
+    ('Khabib', 'Nurmagomedov', 2, NULL, false),
+    ('Harvey', 'Specter', 2, 1, false),
+    ('Bo', 'Jackson', 3, 4, true),
+    ('Lionel', 'Messi', 4, 2, false),
+    ('Achraf', 'Hakimi', 4, NULL, true),
+    ('Marty', 'McFly', 4, NULL, false),
+    ('Pete', 'Castiglione', 5 ,3, false),
+    ('Augustus', 'Gloop', 6, 3, false),
+    ('Hans', 'Landa', 6, 4, false),
+    ('Mike', "Ross", NULL, NULL, false),
+    ('Jose', "Romero", 6, 3, true),
+    ('Elijah', "Roufs", 1, NULL, false),
+    ('Kylian', "Mbappe", 2, 2, false);
 
 INSERT INTO managers (first_name, last_name)
 SELECT first_name, last_name
